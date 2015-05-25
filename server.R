b1a958d067e5008961a492378985a25495672e38
mwalkup55 <mwalkup55@gmail.com>
2015-05-25 18:40:16 -0400

    initial

---
+++ server.R
@@ -0,0 +1,23 @@
+library(leaflet)
+library(shiny)
+
+df <- read.csv("data/data_flat.csv")
+df
+shinyServer(
+  function(input, output) {
+    
+    map <- leaflet() %>% addTiles(urlTemplate = "http://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png", attribution = NULL) %>% setView(lng = -98, lat = 39, zoom = 5)
+    
+    mf <- reactive({
+      df_sub<-df[df$college==as.character(input$teamChooser),]
+      for(i in 1:nrow(df_sub)){
+        map <- map %>% addPolylines(lng = c(df_sub$lon_hs[i],df_sub$lon_col[i]), lat = c(df_sub$lat_hs[i],df_sub$lat_col[i]),popup = df_sub$name[i],weight = 3, color = 'firebrick')
+      }
+      map %>% addMarkers(lng=df_sub$lon_col[1], 
+                                lat=df_sub$lat_col[1],
+                                icon = JS(paste("L.icon({iconUrl: '",df_sub$logo[1],"',iconSize: [40, 40]})",sep='')))
+    })
+    
+    output$myMap <- renderLeaflet(mf()) 
+  }
+)
---
+++ ui.R
@@ -0,0 +1,19 @@
+library(leaflet)
+
+df <- read.csv("data/data_flat.csv")
+shinyUI(fluidPage(
+  tags$head(tags$link(rel='stylesheet', type='text/css', href='styles.css')),
+  
+  titlePanel("Where do Colleges Recruit?"),
+  
+  leafletOutput('myMap',height = "800"),
+  
+  absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                draggable = TRUE, top = 80, left = "auto", right = 30, bottom = "auto",
+                width = 300, height = 75,
+                selectInput("teamChooser", 
+                            label = "Choose a Team",
+                            choices = sort(unique(as.character(df$college))),
+                            selected = "Stanford")
+  )
+))
