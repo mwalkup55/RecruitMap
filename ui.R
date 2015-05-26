67a66ce6bb16fda300953d3f399e3ac0f943e216
mwalkup55 <mwalkup55@gmail.com>
2015-05-25 20:47:21 -0400

    lowered the top thing

--- ui.R
+++ ui.R
@@ -4,12 +4,10 @@ df <- read.csv("data/data_flat.csv")
 shinyUI(fluidPage(
   tags$head(tags$link(rel='stylesheet', type='text/css', href='styles.css')),
   
-  titlePanel("Where do Colleges Recruit?"),
-  
   leafletOutput('myMap',height = "400"),
   
   absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
-                draggable = TRUE, top = 80, left = "auto", right = 30, bottom = "auto",
+                draggable = TRUE, top = 50, left = "auto", right = 30, bottom = "auto",
                 width = 300, height = 75,
                 selectInput("teamChooser", 
                             label = "Choose a Team",
